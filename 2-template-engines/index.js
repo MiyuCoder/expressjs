7024784235549793df6345e979e1490f661e7a26
miyucoder <miyucoder@gmail.com>
2019-07-10 08:28:39 +0700

    delete err file

--- 2-template-engines/index.js
+++ 2-template-engines/index.js
@@ -7,11 +7,18 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index', {
+		name: 'Phan Quoc Trung'
+	});
 });
 
 app.get('/users', function(req, res) {
-	res.send('users list');
+	res.render('users/index', {
+		users: [
+			{ id: 1, name: 'Trung' },
+			{ id: 2, name: 'Thinh' },
+		]
+	});
 })
 
 app.listen(port, function() {
