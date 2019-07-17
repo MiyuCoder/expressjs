cb0838596929429e31f1acf9fc55525bc0ad6db4
miyucoder <miyucoder@gmail.com>
2019-07-17 18:01:18 +0700

    add new file

--- example/db.js
+++ example/db.js
@@ -5,7 +5,9 @@ var adapter = new FileSync('db.json');
 db = low(adapter);
 
 // Set some defaults (required if your JSON file is empty)
-db.defaults({ users: [] })
-  .write();
+db.defaults({
+        users: []
+    })
+    .write();
 
 module.exports = db;
--- example/index.js
+++ example/index.js
@@ -11,19 +11,21 @@ app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 
 app.use(express.static('public'));
 
 // Routes
-app.get('/', function(req, res) {
-  res.render('index', {
-    name: 'AAA'
-  });
+app.get('/', function (req, res) {
+    res.render('index', {
+        name: 'Phan Quoc Trung'
+    });
 });
 
 app.use('/users', userRoute);
 
-app.listen(port, function() {
-  console.log('Server listening on port ' + port);
+app.listen(port, function () {
+    console.log('Server listening on port ' + port);
 });
