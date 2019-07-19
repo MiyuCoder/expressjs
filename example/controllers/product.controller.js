00e077fa362c4b1044c04168a72c291576d1bb22
miyucoder <miyucoder@gmail.com>
2019-07-19 16:42:39 +0700

    20-pagavition dont finish

--- example/controllers/product.controller.js
+++ example/controllers/product.controller.js
@@ -1,5 +1,24 @@
 module.exports.index = function (req, res) {
+    var page = parseInt(req.query.page) || 1; // n
+    var perPage = 8; // x;
+
+    // begin = (n-1) * x;
+    // end = (n-1) * x + x = n * x;
+    // items = array.slice(begin, end)
+
+    var start = (page - 1) * perPage;
+    var end = page * perPage;
+
+    var drop = (page - 1) * perPage;
+    var numberPagination = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+    var perPagination = 2;
+    var startPagination = (page - 1) * perPagination;
+    var endPagination = page * perPagination;
+
     res.render('products/index', {
-        products: db.get('products').value()
+        products: db.get('products').drop(drop).take(perPage).value(),
+        paginations: numberPagination.slice(startPagination, endPagination),
+        paginationsNumber: page
     });
 }
