4025c88f452ebadb9d1fb38b0fdf4324b0f08e58
miyucoder <miyucoder@gmail.com>
2019-07-23 14:50:48 +0700

    add new file

--- example/index.js
+++ example/index.js
@@ -4,11 +4,13 @@ require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var csurf = require('csurf');
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
 var productRoute = require('./routes/product.route');
 var cartRoute = require('./routes/cart.route');
+var transferRoute = require('./routes/transfer.route');
 
 var authMiddleware = require('./middleware/auth.middleware');
 var sessionMiddleware = require('./middleware/session.middleware');
@@ -27,13 +29,16 @@ app.use(bodyParser.urlencoded({
 // using cookie-parser
 app.use(cookieParser(process.env.SESSION_SECRET));
 app.use(sessionMiddleware);
+app.use(csurf({
+    cookie: true
+}));
 
 app.use(express.static('public'));
 
 // Routes
 app.get('/', authMiddleware.requireAuth, function (req, res) {
     res.render('index', {
-        name: 'Phan Quoc Trung'
+        name: 'Hello Coders.Toyko'
     });
 });
 
@@ -41,6 +46,7 @@ app.use('/users', authMiddleware.requireAuth, userRoute);
 app.use('/auth', authRoute);
 app.use('/products', productRoute);
 app.use('/cart', cartRoute);
+app.use('/transfer', authMiddleware.requireAuth, transferRoute);
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
